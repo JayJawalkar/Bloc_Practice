bda937034a420007208c7f758488bbdd8c55e8a2
GauravJawalkar <gauravjawalkar8@gmail.com>
2024-07-24 14:17:52 +0530

    Final

---
+++ lib/inc_dec_page.dart
@@ -0,0 +1,37 @@
+import 'package:bloc_practice/bloc/counter_bloc.dart';
+//import 'package:bloc_practice/cubit/counter_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class IncDecPage extends StatelessWidget {
+  const IncDecPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final counterBloc = BlocProvider.of<CounterBloc>(context);
+
+    return Scaffold(
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              counterBloc.add(CounterIncremented());
+              //counterCubit.Increment();
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              //counterCubit.decrement();
+              counterBloc.add(CounterDecremented());
+            },
+            tooltip: 'Decrement',
+            child: const Icon(Icons.minimize),
+          ),
+        ],
+      ),
+    );
+  }
+}
